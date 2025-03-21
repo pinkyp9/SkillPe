3741bcb826fb782ac0c77beefb8257bdcc5af29b
pinkyp9 <128229194+pinkyp9@users.noreply.github.com>
2025-03-21 18:35:36 +0530

    error removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    images: {
-      domains: ['https://unsplash.com'],
-    },
-  };
-  
 export default nextConfig;
